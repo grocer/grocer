611ca72f7e0fdf38e92b7f54e092aaf20fa55b83
Steven Harman <steveharman@gmail.com>
2012-03-29 17:30:07 -0400

    Use Connection in grocer, which uses SSLConnection

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,4 +1,4 @@
 require_relative "grocer/version"
 require_relative "grocer/notification"
-require_relative "grocer/ssl_connection"
+require_relative "grocer/connection"
 require_relative "grocer/failed_deliver_attempt"

370b54a395fe5fd7a8a5450e6819cd68ce4670ba
Steven Harman <steveharman@gmail.com>
2012-03-30 13:52:22 -0400

    Simplify API; getting Pusher w/Connection via Grocer

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,7 @@ require_relative "grocer/version"
 require_relative "grocer/notification"
 require_relative "grocer/connection"
 require_relative "grocer/feedback"
+require_relative "grocer/pusher"
 
 module Grocer
 
@@ -9,4 +10,8 @@ module Grocer
     ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
   end
 
+  def self.pusher(options)
+    connection = Connection.new(options)
+    Pusher.new(connection)
+  end
 end
--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -2,25 +2,43 @@ require 'spec_helper'
 require 'grocer'
 
 describe Grocer do
-  let(:environment) { nil }
-  before do
-      ENV.stubs(:[]).with('RAILS_ENV').returns(environment)
-      ENV.stubs(:[]).with('RACK_ENV').returns(environment)
-  end
+  subject { described_class }
 
   describe '.env' do
+    let(:environment) { nil }
+    before do
+      ENV.stubs(:[]).with('RAILS_ENV').returns(environment)
+      ENV.stubs(:[]).with('RACK_ENV').returns(environment)
+    end
+
     it 'defaults to development' do
-      Grocer.env.should == 'development'
+      subject.env.should == 'development'
     end
 
     it 'reads RAILS_ENV from ENV' do
       ENV.stubs(:[]).with('RAILS_ENV').returns('staging')
-      Grocer.env.should == 'staging'
+      subject.env.should == 'staging'
     end
 
     it 'reads RACK_ENV from ENV' do
       ENV.stubs(:[]).with('RACK_ENV').returns('staging')
-      Grocer.env.should == 'staging'
+      subject.env.should == 'staging'
+    end
+  end
+
+  describe ".pusher" do
+    let(:connection_options) { stub("connection options") }
+    before do
+      Grocer::Connection.stubs(:new).returns(stub("Connection"))
+    end
+
+    it 'returns a Connection' do
+     subject.pusher(connection_options).should be_a Grocer::Pusher
+    end
+
+    it 'passes the connection options on to the underlying Connection' do
+      subject.pusher(connection_options)
+      Grocer::Connection.should have_received(:new).with(connection_options)
     end
   end
 

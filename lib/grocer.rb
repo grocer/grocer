1d3ff6d98c60c1a6770852c2db532517fa44ff26
Andy Lindeman <andy@highgroove.com>
2012-03-30 10:44:26 -0400

    Adds missing require for Grocer::Feedback

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,4 +1,4 @@
 require_relative "grocer/version"
 require_relative "grocer/notification"
 require_relative "grocer/connection"
-require_relative "grocer/failed_delivery_attempt"
+require_relative "grocer/feedback"

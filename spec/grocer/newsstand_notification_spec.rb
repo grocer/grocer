aabfde47f6ddb3eb865510d6cb57e547cfc9de29
Khaled alHabache <khellls@gmail.com>
2013-03-14 01:19:53 +0300

    fix newsstand notification payload

--- spec/grocer/newsstand_notification_spec.rb
+++ spec/grocer/newsstand_notification_spec.rb
@@ -5,17 +5,17 @@ require 'grocer/shared_examples_for_notifications'
 describe Grocer::NewsstandNotification do
   describe 'binary format' do
     let(:payload_options) { Hash.new }
-    let(:payload_from_bytes) { notification.to_bytes[45..-1] }
     let(:payload_dictionary_from_bytes) { JSON.parse(payload_from_bytes, symbolize_names: true) }
+    let(:payload_from_bytes) { notification.to_bytes[45..-1] }
 
     include_examples 'a notification'
 
     it 'does require a payload' do
-      expect(payload_dictionary_from_bytes[:'content-available']).to_not be_nil
+      expect(payload_dictionary_from_bytes[:aps]).to_not be_empty
     end
 
-    it 'has a content-available hash' do
-      expect(payload_dictionary_from_bytes[:'content-available']).to eq(1)
+    it 'encodes content-available as part of the payload' do
+      expect(payload_dictionary_from_bytes[:aps][:'content-available']).to eq(1)
     end
 
   end
